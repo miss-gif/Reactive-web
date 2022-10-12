caf977e40385952f7c064de1d5dadd857cd5dd28
miss <ddc.akari@gmail.com>
2022-10-12 18:25:30 +0900

    fix

--- main.js
+++ main.js
@@ -1,8 +1,25 @@
-// 내비게이션 버튼 토글
+// 내비게이션 접기 버튼, 토글 기능
 const navBtn = document.querySelector(".title .btn");
 
-navBtn.addEventListener("click", function () {
+navBtn.addEventListener("click", function (e) {
   const nav = document.querySelector(".nav");
+  e.preventDefault();
   nav.classList.toggle("hidden");
   navBtn.classList.toggle("rotate");
+});
+
+
+// 스와이퍼 슬라이드
+
+var swiper = new Swiper(".swiper", {
+  cssMode: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  mousewheel: true,
+  keyboard: true,
 });
